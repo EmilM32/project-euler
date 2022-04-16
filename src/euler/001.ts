8aee41b14fd8ed3959fd6959f78bf39ace9632dd
Emil Majer <emil.majer@strix.net>
2022-04-16 12:57:30 +0200

    add select list for projects

---
+++ src/euler/001.ts
@@ -0,0 +1,16 @@
+import chalk from 'chalk'
+
+/**
+ * Problem 1
+ * Multiples of 3 or 5
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * @see {@link https://projecteuler.net/problem=1}
+ */
+
+console.log(chalk.bold.cyan('Problem 1'))
+console.log(chalk.bold.magenta('Multiples of 3 or 5'), '\n')
+
+console.log('result of 001')
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import cliSelect from 'cli-select'
 import chalk from 'chalk'
+import fs from 'fs'
+import path from 'path'
+import { fork } from 'child_process'
 
-cliSelect({
-  values: ['Major', 'Minor', 'Patch'],
-  valueRenderer: (value, selected) => {
-    if (selected) {
-      return chalk.underline(value)
-    }
+const DIRECTORY_PATH = './src/euler/'
 
-    return value
-  },
-}).then((selected) => {
-  console.log(selected)
+const availableFiles: string[] = []
+
+console.log(chalk.green('Select project to run:'), '\n')
+
+fs.readdir(DIRECTORY_PATH, (err, files) => {
+  if (err) throw new Error(err.message)
+
+  files.forEach((file) => {
+    availableFiles.push(file.split('.')[0])
+  })
+
+  cliSelect({
+    values: availableFiles,
+    valueRenderer: (value, selected) => {
+      if (selected) {
+        return chalk.underline(value)
+      }
+
+      return value
+    },
+  }).then((selected) => {
+    const filePath = path.join(
+      __dirname,
+      `euler/${selected.value}.ts`,
+    )
+
+    const child = fork(path.join(filePath))
+    child.on('error', (err) => {
+      throw new Error(err.message)
+    })
+  })
 })
