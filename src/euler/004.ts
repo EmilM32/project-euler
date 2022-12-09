78d8b0cc5b152beb1fa8dc6106d723fa6069f162
Emil Majer <emil.majer@strix.net>
2022-12-09 13:25:33 +0100

    resolve problem 4

--- src/euler/004.ts
+++ src/euler/004.ts
@@ -13,4 +13,30 @@ import { logTitle, logSubtitle, logResult } from '../utils/logs'
 logTitle('Problem 4')
 logSubtitle('Largest palindrome product')
 
-logResult(`Result of problem 4:`)
+const isPalindrome = (value: number): boolean => {
+  const numberAsString = value.toString()
+  const reversedNumber = numberAsString.split('').reverse().join('')
+
+  return numberAsString === reversedNumber
+}
+
+const largestPalindromic = (digits: number): number => {
+  const largestNumber = Math.pow(10, digits) - 1
+  const smallestNumber = Math.pow(10, digits - 1)
+  let largestPalindrome = 0
+
+  for (let i = largestNumber; i > smallestNumber; i--) {
+    for (let j = largestNumber; j > smallestNumber; j--) {
+      const number = i * j
+      if (isPalindrome(number) && number > largestPalindrome) {
+        largestPalindrome = number
+      }
+    }
+  }
+
+  return largestPalindrome
+}
+
+const result = largestPalindromic(3)
+
+logResult(`Result of problem 4: ${result}`)
